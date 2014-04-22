edb9018b503063eb410f64ce6046ffaac9e39dfc
frankie87 <frankie@sampo.ru>
2014-04-22 22:15:47 +0200

    Assignment 2 for r progr

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function implements matrix inversion and caching
+## as well as get and set this stuff
 
-## Write a short comment describing this function
+## make inverse matrix and create getters and setters
 
 makeCacheMatrix <- function(x = matrix()) {
+s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## If there is a cached copy of inversed matrix - get it, otherwise create new
+## and add it to cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+			 s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
